0e479921b0c37302456be012dec01d7374ef16a4
Catalin Micu <catalinmicu98@gmail.com>
2022-01-19 23:42:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+import googlemaps
+
+gmaps = googlemaps.Client(key='AIzaSyBZ7l2jGuWaRGn2rm1TKhW3GjmraDCpEgA')
+
+if __name__ == '__main__':
+    print('rotodendron')
+    geo = gmaps.geocode('86 Sergent Ilie Petre')
+    directions = gmaps.directions('Focsani, fratiei 3', 'Craiova, 1 decembrie 1918 23', mode='transit',
+                                  departure_time=datetime.now())
+    a=2
---
+++ models/__init__.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Row
+from sqlalchemy.orm import sessionmaker, declarative_base
+from flask_server.cutom_logger import create_logger
+
+con_url = 'postgresql://postgres:password@localhost:5432/meal_builder'
+engine = create_engine(con_url)
+Session = sessionmaker(bind=engine)
+
+
+Base = declarative_base()
+logger = create_logger(__name__, '%(name)s.%(module)s.%(funcName)s -> %(levelname)s: %(message)s')
+
+
+class BaseTable(Base):
+    __abstract__ = True
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.session = Session()
+
+    @staticmethod
+    def _transform_row_into_dict(r: Row, columns: []) -> dict:
+        result = dict()
+        for c in columns:
+            result[c] = getattr(r._data[0], c)
+
+        return result
+
+    @staticmethod
+    def _transform_table_obj_into_dict(t, columns) -> dict:
+        result = dict()
+        for c in columns:
+            result[c] = getattr(t, c)
+
+        return result
